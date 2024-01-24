4536673a408af46ce8e7704ca5d24fb1d6d2715b
Hardik0101 <hardik.prajapati@seaflux.tech>
2024-01-24 17:42:20 +0530

    Added Button styles

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, View } from "react-native";
 import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
-  return <StartGameScreen />;
+  return;
+  <View style={styles.rootScreen}>
+    <StartGameScreen />;
+  </View>;
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "red",
+  rootScreen: {
+    backgroundColor: "#ddb52f",
   },
 });
--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,11 +1,46 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 function PrimaryButton({ children }) {
+  function pressHandler() {
+    console.log("Pressed");
+  }
   return (
-    <View>
-      <Text>{children}</Text>
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }
+        onPress={pressHandler}
+        android_ripple={{ color: "#640233" }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
     </View>
   );
 }
 
 export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  buttonText: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+  },
+  buttonInnerContainer: {
+    backgroundColor: "#72031c",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
